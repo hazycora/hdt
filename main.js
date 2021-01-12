aef18111abe1b513ac920c89427f868f668d44ca
hazycora <hazycora@gmail.com>
2021-01-12 06:47:34 -0600

    fixed word wrap

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 260,
+    width: 280,
     height: 140,
     minHeight: 160,
-    minWidth: 260,
+    minWidth: 280,
     backgroundColor: '#302b3b',
     //frame: false,
     webPreferences: {
